44f9d6731e370d815b2a543229a56c7ad0683aad
guilherme <guifracaro@outlook.com>
2022-08-03 14:02:43 -0300

    up

---
+++ pythonexes/ex077.py
@@ -0,0 +1,6 @@
+palavras = ('aprender', 'programar', 'linguagem',
+            'python', 'curso', 'gratis',
+            'estudar', 'praticar', 'trabalhar',
+            'mercado', 'programador', 'futuro')
+for p in palavras:
+    print('{}'.format(p), end=', ')
